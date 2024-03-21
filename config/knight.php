6ded88ec42c8a0ddceda40b657b539497a3fe14d
hugh.li <hugh.li@foxmail.com>
2024-03-21 13:19:18 +0800

    feat: 添加preStop handler

--- config/knight.php
+++ config/knight.php
@@ -5,9 +5,9 @@ return [
         'route_prefix' => 'ahn8a5cohlRai4AiTei3oeaiei7tipahVa0shei0Xea4',
     ],
     'request' => [
-        'route_prefix' => 'YeeNge6Ere8aiGholai7shaNathooc5IOhP2ahh4Reiq',
+        'route_prefix' => 'ahn8a5cohlRai4AiTei3oeaiei7tipahVa0shei0Xea4',
     ],
     'phpinfo' => [
-        'route_prefix' => 'hee5ooS2heid1Thoka7aiJeeAeG0ohGaMei7euToaine',
+        'route_prefix' => 'ahn8a5cohlRai4AiTei3oeaiei7tipahVa0shei0Xea4',
     ],
 ];
