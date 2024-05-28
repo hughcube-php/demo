522a8aaea1e62699d033e40d1e55cd11a14cca4b
hugh.li <hugh.li@foxmail.com>
2024-05-28 19:38:48 +0800

    bug: fix bug

--- config/app.php
+++ config/app.php
@@ -166,7 +166,7 @@ return [
     'providers' => [
         Illuminate\Auth\AuthServiceProvider::class,
         #Illuminate\Broadcasting\BroadcastServiceProvider::class,
-        #Illuminate\Bus\BusServiceProvider::class,
+        Illuminate\Bus\BusServiceProvider::class,
         Illuminate\Cache\CacheServiceProvider::class,
         #Illuminate\Foundation\Providers\ConsoleSupportServiceProvider::class,
         #Illuminate\Cookie\CookieServiceProvider::class,
