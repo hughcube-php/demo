8a7f81e43cd9d00ba92715f68fac35b69de896d8
hugh.li <hugh.li@foxmail.com>
2024-08-22 16:37:57 +0800

    debug: timeout

--- config/octane.php
+++ config/octane.php
@@ -188,7 +188,7 @@ return [
     |
     */
 
-    'max_execution_time' => 30, #intval(env('OCTANE_MAX_EXECUTION_TIME', 30)),
+    'max_execution_time' => 10, #intval(env('OCTANE_MAX_EXECUTION_TIME', 30)),
 
     'swoole' => [
         'options' => [
